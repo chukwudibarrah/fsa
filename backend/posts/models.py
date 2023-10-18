16858eb37a1c3f9b145af6e4da8d84354bc4b6aa
chukwudibarrah <118562380+chukwudibarrah@users.noreply.github.com>
2023-10-18 16:25:00 +0100

    first commit

---
+++ backend/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(default='default.png', upload_to='pimages', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+
+    class Meta:
+        ordering = ('-date',)
+
+    def __str__(self):
+        return f"Post: {self.title}"
+    
